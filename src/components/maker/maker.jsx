0da59b5bc013883fecec73dccacf340596d285bc
JiHwanLee <kdgosu53@gmail.com>
2022-07-17 19:01:07 +0900

    add maker and preview 레이아웃 추가

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Editor from "../editor/editor";
 
@@ -8,6 +8,41 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      name: "Ellie",
+      company: "Samsung",
+      theme: "light",
+      title: "SoftWare Engineer",
+      email: "ellie@naver.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: "ellie.png",
+    },
+    {
+      id: 2,
+      name: "Ellie2",
+      company: "Samsung2",
+      theme: "light2",
+      title: "SoftWare Engineer2",
+      email: "ellie@naver.com2",
+      message: "go for it2",
+      fileName: "ellie2",
+      fileURL: "ellie.png2",
+    },
+    {
+      id: 3,
+      name: "Ellie3",
+      company: "Samsung3",
+      theme: "light3",
+      title: "SoftWare Engineer3",
+      email: "ellie@naver.com3",
+      message: "go for it3",
+      fileName: "ellie3",
+      fileURL: "ellie.png3",
+    },
+  ]);
   const { id } = useLocation();
   const navigate = useNavigate();
 
@@ -27,8 +62,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
